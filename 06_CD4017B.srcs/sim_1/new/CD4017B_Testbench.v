82a70c2c6fb3a7cb2a3c2e8affd69a92b9a19d47
ngodonghai081198 <haingodong1998@gmail.com>
2025-03-13 01:04:05 +0700

    modified:   06_CD4017B.cache/wt/project.wpc 	modified:   06_CD4017B.cache/wt/synthesis.wdf 	new file:   06_CD4017B.runs/.jobs/vrs_config_29.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_30.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_31.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_32.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_33.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_34.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_35.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_36.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_37.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_38.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_39.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_40.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_41.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_42.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_43.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_44.xml 	new file:   06_CD4017B.runs/.jobs/vrs_config_45.xml 	modified:   06_CD4017B.runs/synth_1/.vivado.begin.rst 	modified:   06_CD4017B.runs/synth_1/CD4017B.dcp 	modified:   06_CD4017B.runs/synth_1/CD4017B.tcl 	modified:   06_CD4017B.runs/synth_1/CD4017B.vds 	modified:   06_CD4017B.runs/synth_1/CD4017B_utilization_synth.pb 	modified:   06_CD4017B.runs/synth_1/CD4017B_utilization_synth.rpt 	modified:   06_CD4017B.runs/synth_1/gen_run.xml 	modified:   06_CD4017B.runs/synth_1/project.wdf 	modified:   06_CD4017B.runs/synth_1/runme.log 	modified:   06_CD4017B.runs/synth_1/vivado.jou 	modified:   06_CD4017B.runs/synth_1/vivado.pb 	modified:   06_CD4017B.sim/sim_1/behav/xsim/CD4017B_Testbench_behav.wdb 	modified:   06_CD4017B.sim/sim_1/behav/xsim/compile.sh 	modified:   06_CD4017B.sim/sim_1/behav/xsim/elaborate.sh 	modified:   06_CD4017B.sim/sim_1/behav/xsim/simulate.sh 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/CD4017B_Testbench_behav/obj/xsim_0.lnx64.o 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/CD4017B_Testbench_behav/obj/xsim_1.c 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/CD4017B_Testbench_behav/obj/xsim_1.lnx64.o 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/CD4017B_Testbench_behav/xsim.dbg 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/CD4017B_Testbench_behav/xsim.mem 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/CD4017B_Testbench_behav/xsim.reloc 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/CD4017B_Testbench_behav/xsim.rlx 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/CD4017B_Testbench_behav/xsim.rtti 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/CD4017B_Testbench_behav/xsim.xdbg 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/CD4017B_Testbench_behav/xsimk 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/CD4017B_Testbench_behav/xsimkernel.log 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/xil_defaultlib/@c@d4017@b.sdb 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/xil_defaultlib/@c@d4017@b_@testbench.sdb 	modified:   06_CD4017B.sim/sim_1/behav/xsim/xsim.dir/xil_defaultlib/xil_defaultlib.rlx 	modified:   06_CD4017B.srcs/sim_1/new/CD4017B_Testbench.v 	modified:   06_CD4017B.srcs/sources_1/new/CD4017B.v 	modified:   06_CD4017B.srcs/utils_1/imports/synth_1/CD4017B.dcp 	modified:   06_CD4017B.xpr

--- 06_CD4017B.srcs/sim_1/new/CD4017B_Testbench.v
+++ 06_CD4017B.srcs/sim_1/new/CD4017B_Testbench.v
@@ -24,25 +24,35 @@ module CD4017B_Testbench(
 
     );
     
-    reg D;
     reg rst;
     reg clk;
     reg clk_en;
-    wire D0;
-    wire D5;
+    wire out0;
+    wire out1;
+    wire out2;
+    wire out3;
+    wire out4;
+    wire out5;
+    wire out6;
+    wire out7;
+    wire out8;
+    wire out9;
+    wire Cout;
         
-    CD4017B UI(D, rst, clk, clk_en, D0, D5);
+    CD4017B UI(rst, clk, clk_en, out0, out1, out2, out3, out4, out5, out6, out7, out8, out9, Cout);
     
     always #25 clk = !clk;
-    always #50 D = !D;
     
     initial begin
-        D = 0;
         rst = 1;
         clk = 0;
         clk_en = 0; 
         #25;
         rst = 0;
+        #650;
+        clk_en = 1;
+        #25;
+        clk_en = 0;
     end
 
 //    always #25 clk = !clk;
--- 06_CD4017B.srcs/sources_1/new/CD4017B.v
+++ 06_CD4017B.srcs/sources_1/new/CD4017B.v
@@ -21,34 +21,82 @@
 
 
 module CD4017B(
-    input wire D,
     input wire rst,
     input wire clk,
     input wire clk_en,
-    output reg D0,
-    output reg D5
+    output reg out0,
+    output reg out1,
+    output reg out2,
+    output reg out3,
+    output reg out4,
+    output reg out5,
+    output reg out6,
+    output reg out7,
+    output reg out8,
+    output reg out9,
+    output reg Cout
     );
     
-    wire temp1, temp2, temp3;
+    wire D1, D2, D3, D4, D5;
+    wire temp1, temp2, temp3, temp4;
+    reg Q1, Q1bar, Q2, Q2bar, Q3, Q3bar, Q4, Q4bar, Q5, Q5bar;
     assign temp1 = !clk_en;
     assign temp2 = !rst;
     assign temp3 = !temp2;
-    
+    assign D1 = Q5bar;    
+    assign temp4 = !(Q1 | Q2bar | Q3);
+    assign temp5 = !(temp4 | Q2bar);
+    assign D2 = Q1;
+    assign D3 = temp5;
+    assign D4 = Q3;
+    assign D5 = Q4;
 
     always @(posedge clk) begin
         if (temp1) begin
             if (temp3) begin
-                D0 = 0;
-                D5 = 1;
+                Q1 = 0;
+                Q1bar = 1;
+                Q2 = 0;
+                Q2bar = 1;
+                Q3 = 0;
+                Q3bar = 1;
+                Q4 = 0;
+                Q4bar = 1;
+                Q5 = 0;
+                Q5bar = 1;
             end
             else begin
-                D0 = D;
-                D5 = !D;
+                Q1      <= D1;
+                Q1bar   <= !D1;
+                Q2      <= D2;
+                Q2bar   <= !D2;
+                Q3      <= D3;
+                Q3bar   <= !D3;
+                Q4      <= D4;
+                Q4bar   <= !D4;
+                Q5      <= D5;
+                Q5bar   <= !D5;
+                Cout    <= !Q5;
+                out0    <= !Q1 & !Q5;
+                out1    <= !Q1bar & !Q2;
+                out2    <= !Q2bar & !Q3;
+                out3    <= !Q3bar & !Q4;
+                out4    <= !Q4bar & !Q5;
+                out5    <= !Q5bar & !Q1bar;
+                out6    <= !Q1 & !Q2bar;
+                out7    <= !Q2 & !Q3bar;
+                out8    <= !Q3 & !Q4bar;
+                out9    <= !Q4 & !Q5bar;
+                
             end
         end
     end
     
 endmodule
+
+
+
+
 
 
 
